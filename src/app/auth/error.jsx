b43f92ada016f37b3a8b3a2a57e7d9bd09eddc8d
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-06-08 20:33:12 +0300

    fix

--- src/app/auth/error.jsx
+++ src/app/auth/error.jsx
@@ -4,9 +4,11 @@ import { AiOutlineAlert } from "react-icons/ai";
 function Error() {
   return (
     <div className="mt-8">
-      <h2 className="text-xl font-semibold text-red-500">Something Went Wrong</h2>
+      <h2 className="text-xl font-semibold text-red-500">
+        Something Went Wrong
+      </h2>
       <p className="text-base text-gray-500">
-        We're sorry, but something went wrong. Please try again later.
+        We&apos;re sorry, but something went wrong. Please try again later.
       </p>
       <AiOutlineAlert className="mx-auto my-4" />
     </div>
@@ -14,4 +16,3 @@ function Error() {
 }
 
 export default Error;
-
